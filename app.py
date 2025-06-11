048efd597ebde1e001c0c15d9ae4debfba0d4f8f
Nathwach1 <161469220+Nathwach1@users.noreply.github.com>
2025-06-11 02:00:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,10 +23,11 @@ DB = "book_bank.db"
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'wachiranathanael7@gmail.com'
-app.config['MAIL_PASSWORD'] = 'rkeymznwntobxbbe'
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 
-app.config['MAIL_DEFAULT_SENDER'] = 'wachiranathanael7@gmail.com'
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
+
 
 
 mail = Mail(app)
